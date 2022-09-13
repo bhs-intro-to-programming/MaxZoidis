adb5d6f891b870268d89d290fb28013fd7c4043e
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-09-13 15:08:51 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -32,6 +32,6 @@
 // Feel free to play around in the REPL to make sure you understand how this
 // operator works.
 
-const pigLatin = (s) => s.substring(s.search(/[aeiou]/)) + s.substring(0, s.search(/[aeiou]/)) + 'ay';
+const pigLatin = (b) => b.substring(b.search(/[aeiou]/)) + b.substring(0, b.search(/[aeiou]/)) + 'ay';
 
 const advancedPigLatin = (b) => b.search(/[aeiou]/) === 0 ? + b+ "way" : pigLatin(b);
