f1f85c16aa7a914db726382c68047a99e809b67c
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-09-21 15:00:06 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -24,13 +24,13 @@
  *  clear()
  */
 
-let radius = 12
+*/let radius = 12
 let diameter = radius * 2
 let x = 0
 while (x < width - diameter) {
   drawFilledCircle(diameter + x, height / 2, radius, 'red')
   x += diameter
-}
+}*/
 
 /*let radius = 12
 let diameter = radius * 2
