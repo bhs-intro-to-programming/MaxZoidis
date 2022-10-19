8402a4da61ba68f38897975791685e427ee9d2cc
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-10-19 14:57:30 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,7 +1,7 @@
 const countTens = (arrayofnumbers) => {
   let numbertens = 0
-  for(let i = 0; i < arrayofnumbers.length; i++) {
-    if(arrayofnumbers[i]===10){
+  for (let i = 0; i < arrayofnumbers.length; i++) {
+    if (arrayofnumbers[i] === 10) {
       numbertens++
     }
   }
