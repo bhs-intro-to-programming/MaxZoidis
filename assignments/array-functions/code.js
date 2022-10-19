0bf8fde1079cb89331984607d1d4ab1a24dbb3eb
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-10-19 15:04:30 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,12 +1,12 @@
-const countTens = (arrayofnumbers) => {
-  let numbertens = 0
-  for (let i = 0; i < arrayofnumbers.length; i++) {
-    if (arrayofnumbers[i] === 10) {
-      numbertens++
-    }
-  }
-  return numbertens
-}
+//const countTens = (arrayofnumbers) => {
+ // let numbertens = 0
+  //for (let i = 0; i < arrayofnumbers.length; i++) {
+  //  if (arrayofnumbers[i] === 10) {
+  //    numbertens++
+  //  }
+ // }
+  //return numbertens
+//}
 
 const sum = arrayofnumbers => {
   let s = 0
