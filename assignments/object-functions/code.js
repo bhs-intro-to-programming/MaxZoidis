50aa9a4e468f0ace869aae11009205e085e8b792
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-11-08 14:50:42 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -13,3 +13,7 @@ const emptyObject = () => {
 const distance = (p1, p2) => {
   return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
 }
+
+const midpoint = (p1, p2) => {
+  return (p1.x - p2.x) * 2 + (p1.y - p2.y)*2
+}
