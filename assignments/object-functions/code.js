475fbb6ba0bb3695520dd79a9c3720da62d03885
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-11-08 15:28:26 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -17,3 +17,12 @@ const distance = (p1, p2) => {
 const midpoint = (p1, p2) => {
   return { x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) /2 }
 }
+
+const sumSalaries = (array) => {
+  let p=0
+  for(const element of array ) {
+  p+= element.salary 
+  } 
+  return p 
+}
+
