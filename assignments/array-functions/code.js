2fa3fe14cd29bf727b60df331e4093f891e8f7e6
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-11-13 17:25:13 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,13 +1,10 @@
-const countTens = (arrayofnumbers) => {
-  let numbertens = 0
-  for (let i = 0; i < arrayofnumbers.length; i++) {
-    if (arrayofnumbers[i] === 10) {
-      numbertens++
+const countTens = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    if array[i] === 10{
+      return true
     }
   }
-  return numbertens
 }
-
 const sum = (arrayofnumbers) => {
   let s = 0
   for (let i = 0; i < arrayofnumbers.length; i++) {
@@ -45,10 +42,3 @@ const pyramid = (array) => {
 }
 
 
-const countTens = (array) => {
-  for(let i=0; i<array.length;i++) {
-    if array[i]===10{
-      return true
-    }
-  }
-}
