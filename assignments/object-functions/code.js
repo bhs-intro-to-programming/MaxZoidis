7711486c0396b2d2728033c067437ca31f415f34
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-11-14 22:19:44 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -51,29 +51,3 @@ const summarizeBooks = (array) => {
   }
 }
 
-
-
-const getX = (object) => {
-  return object.x
-}
-
-const point = (x, y) => {
-  return { x: x, y: y }
-}
-
-const emptyObject = () => {
-  return {}
-}
-
-const distance = (p1, p2) => {
-  return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
-}
-
-const midpoint = (p1, p2) => {
-  return {
-    x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2
-  }
-
-  const sumSalaries = (arrayofobjects) => {
-    return 
-  }
