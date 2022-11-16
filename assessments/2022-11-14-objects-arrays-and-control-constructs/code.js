be107bf5962438712879c5f2a00dbd972d74655e
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-11-16 15:12:12 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -2,28 +2,21 @@
 // should save you some time.
 
 const area = (rect) => {
-  return rect.width * rect.height
 };
 
 const higherPaid = (e1, e2) => {
-  for ()
-
- }
 };
 
 const isSamePoint = (p1, p2) => {
-  return pl.x
 };
 
 const totalWithTip = (bill, tipPercentage) => {
 };
 
 const isWinner = (player) => {
-  const element of array
 };
 
 const updateWins = (players) => {
-
 };
 
 const bigWinners = (players) => {
