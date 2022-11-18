98fdbc0dc2e920a02eebd236609a2d060c12a2d6
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-11-18 15:12:25 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -38,8 +38,8 @@ const anyOverOneHundred = (arrayofnumbers) => {
 
 const pyramid = array => {
   let p = []
-  for (let i = 0; i < array.length; i++) {
+  for (let p = 0; p < array.length; p++) {
     return array.push[p++]
   }
-  return i++
+  return p++
 }
