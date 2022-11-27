3a478bf6cc88f0881e86f88ec4837a8a85e9d45c
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-11-26 16:44:03 -0800

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -8,3 +8,34 @@ const distance = (a, b) => Math.abs(a - b)
 const manhattanDistance = (x1, y1, x2, y2) => distance(x1, x2) + distance(y1, y2);
 const mod = (a, b) => a % b
 const euclideanDistance = (x1, y1, x2, y2) => Math.sqrt(Math.abs(x1 - x2) ** 2 + Math.abs(y1 - y2) ** 2)
+
+con
+
+
+
+const add = (a,b)=> {
+  return a+b
+}
+
+const subtract = (a,b)=> {
+  return a-b
+}
+
+const multiply = (a,b)=> {
+  return a*b
+}
+const divide = (a,b)=> {
+  return a/b
+}
+const mod = (a,b)=> {
+  return a % b
+}
+const averageOfTwo = (a,b,)=> {
+  return (a+b)/2
+}
+const averageOfThree = (a,b,c)=> {
+  return (a+b+c)/3
+}
+const distance = (a,b)=>{
+  return
+}
