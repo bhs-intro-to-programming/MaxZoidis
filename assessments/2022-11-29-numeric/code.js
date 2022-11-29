8670220ea17af47d9db38a7e7ee2dc907e224896
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-11-29 14:51:47 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -3,7 +3,7 @@ const totalEggs = (hardBoiled, softBoiled) => {
 }
 
 const chocolatesPerPerson = (numChocolates, numPeople) => {
-  return numChocolates % numPeople
+  return Math.floor(numChocolates % numPeople)
 }
 
 const extraChocolates = (numChocolates, numPeople) => {
