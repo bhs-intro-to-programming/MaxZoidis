c5d0264b59efbde5217b0912fbc99f8006dad5ae
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-03 21:22:49 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -1,7 +1,3 @@
-const capitalize = (string) => {
-  return string.length(0).toUpperCase()
-}
-
-const firstAndLast = (string) => {
-  return s.substring[0,5]
+const capitilaze = (string) => {
+  return string.toUpperCase()
 }
