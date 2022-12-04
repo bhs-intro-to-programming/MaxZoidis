b3d5dbe3b8b7068a69995110dc7cee512d968b50
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-03 23:03:54 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -35,6 +35,6 @@ const everyOther = (s) => {
   return s.slice(0) + s.slice(2) + s.slice(4)
 }
 
-const firstHalf = (foobar) => {
-  return foobar.slice(0,3)
+const firstHalf = () => {
+  return s.slice(s.length - s.length/2)
 }
