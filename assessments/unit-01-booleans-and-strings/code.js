0115804d7f341098283e5bf92f15ba55301ea6e8
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-04 21:41:20 -0800

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -40,8 +40,8 @@ const firstAndLast = (string) => {
 }
 
 
-const swapFrontAndBack = (string) => {
-  return
+const swapFrontAndBack = (s) => {
+  return s.substring(s.length / 2) + s.substring(0, s.length / 2)
 }
 
 
