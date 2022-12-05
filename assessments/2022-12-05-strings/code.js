7fac8a52c39f52cad5a66d3132e1cca39e4488ba
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-05 15:36:41 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -31,3 +31,7 @@ const slug = (s1, s2, s3) => {
 const capitalize = (s) => {
   return s.substring(0,1).toUpperCase() + s.substring(1,s.length).toLowerCase()
 }
+
+const capitalizeName = (firstName) => {
+  return capitalize(firstName)
+}
