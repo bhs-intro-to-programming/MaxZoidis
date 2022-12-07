982da7baf42b1ca633ce65b310b4ae94ac63a90f
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-06 20:02:42 -0800

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -70,3 +70,14 @@ const timesTable = (n) => {
    }
  }
 }
+
+const containsX = (s) => {
+  for(let i =0; i < s.length;i++) {
+    if (s[i] === 'x') {
+      return true
+    }
+  }
+  else {
+    return false
+  }
+}
