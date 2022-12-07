e47044625afa25c471e0be06d994f27d9cf9f51b
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-06 20:15:57 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -1,3 +1,12 @@
 const area = (rect) => {
   return rect.width * rect.height
 }
+
+const higherPaid = (e1,e2) => {
+  if (e1.salary > e2.salary) {
+    return e1
+  }
+  if (e2.salary > e1.salary) {
+    return e2
+  }
+}
