7e9ba69a4d27437f3cbcbb8d4e81410daafa03b6
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-06 21:05:33 -0800

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -44,46 +44,11 @@ const aFartherThanB = (a, b, c) => {
 
 }
 
-const yesIfEven = (n) => {
-  if (n % 2 === 0) {
-    return 'yes'
-  } else {
-    return 'no'
-  }
-};
-
-const countXs = (s) => {
-  let count = 0
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === 'x') {
-      count++
-    }
-
-  }
-  return count
-}
-
-const timesTable = (n) => {
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= n; i++) {
-      emit(i, j, i * j)
-    }
-  }
-}
-
 const containsX = (s) => {
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === 'x') {
-      return true
-    }
-  }
-  return false 
-}
-
-const sumSquares = (n) => {
-  let sum = 0
-  for(let i = 0; i<n;i++) {
-    sum += i ** 2
-  }
-  return sum
+ for(let i = 0; i< s.length; i++) {
+   if (s[i] === 'x') {
+     return true
+   }
+ }
+ return false
 }
