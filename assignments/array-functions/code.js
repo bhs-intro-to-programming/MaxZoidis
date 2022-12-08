d1bfbbd4e39984044c2ed4015cfbfcc91d80477b
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-07 19:53:02 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -27,10 +27,10 @@ const evens = (array) => {
 }
 
 const ayOverOneHundred = (array) => {
-  for(let i = 0; i < array.length; i++) {
-if (array[i] > 100) {
-  return true
-}
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 100) {
+      return true
+    }
   }
   return false
 }
