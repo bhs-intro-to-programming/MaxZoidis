1191bb8040386eb24896df141e95e3c18aedaefb
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-07 19:39:02 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -45,3 +45,11 @@ const pyramid = array => {
 }
 
 
+const countTens = (array) => {
+  let count = 0
+  for(let i = 0; i< array.length;i++) {
+    if(array(i) === 10)
+    count++
+  }
+  return count
+}
