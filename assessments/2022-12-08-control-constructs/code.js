dc3e6ef8bc65d62bdc2d080da1a94872b09f44d6
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-08 15:05:25 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -19,7 +19,7 @@ const countXs = (s) => {
 const timesTable = (n) => {
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n; j++) {
-      emit(i, j, i * j)
+     emit(i, j, i * j)
     }
   }
 }
