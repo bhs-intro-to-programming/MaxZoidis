7eb95640fee82dcc8b7d35fad23702b73bf0dc9e
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-07 20:07:04 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -1,27 +1,32 @@
+// For a change of pace, I'm providing you with all the function skeletons. This
+// should save you some time.
+
 const area = (rect) => {
-  return rect.width * rect.height
-}
+}; 
 
-const higherPaid = (e1,e2) => {
-  if (e1.salary > e2.salary) {
-    return e1
-  }
-  if (e2.salary > e1.salary) {
-    return e2
-  }
-}
+const higherPaid = (e1, e2) => {
+};
 
-const isSamePoint = (p1,p2) => {
-  return (p1.x === p2.x && p2.y === p1.y) 
-
-}
+const isSamePoint = (p1, p2) => {
+};
 
 const totalWithTip = (bill, tipPercentage) => {
-  const tip = bill.subtotal * tipPercentage
-  return {
-    subtotal: bill.subtotal,
-    tip: tip,
-    total: bill.subtotal + tip
+};
 
-  }
-}
+const isWinner = (player) => {
+};
+
+const updateWins = (players) => {
+};
+
+const bigWinners = (players) => {
+};
+
+const fillTimesTable = (table) => {
+};
+
+const sums = (n) => {
+};
+
+const rule110 = (cells) => {
+};
