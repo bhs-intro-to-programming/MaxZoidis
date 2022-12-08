f549148934d2b5741b2171a3b1f4e696bf08fe6a
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-07 19:48:40 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -20,7 +20,8 @@ const evens = (array) => {
   let elements = []
   for(let i = 0; i< array.length;i++) {
     if (array[i] % 2 === 0) {
-      return  elements.push(array)
+      return  elements.push(array[i]
     }
   }
+  return elements
 }
