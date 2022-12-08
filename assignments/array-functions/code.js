2219d33c4e7db7eb8c4432a66f57f0c46a3903cb
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-07 19:52:50 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,16 +1,16 @@
 
 const countTens = (array) => {
   let count = 0
-  for(let i = 0; i< array.length;i++) {
-    if(array[i] === 10)
-    count++
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 10)
+      count++
   }
   return count
 }
 
 const sum = (array) => {
   let sum = 0
-  for(let i = 0;i< array.length;i++) {
+  for (let i = 0; i < array.length; i++) {
     sum += array[i]
   }
   return sum
@@ -18,10 +18,19 @@ const sum = (array) => {
 
 const evens = (array) => {
   let elements = []
-  for(let i = 0; i< array.length;i++) {
+  for (let i = 0; i < array.length; i++) {
     if (array[i] % 2 === 0) {
-        elements.push(array[i])
+      elements.push(array[i])
     }
   }
   return elements
+}
+
+const ayOverOneHundred = (array) => {
+  for(let i = 0; i < array.length; i++) {
+if (array[i] > 100) {
+  return true
+}
+  }
+  return false
 }
