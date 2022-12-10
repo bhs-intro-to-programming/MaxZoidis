605163dce90b022738cd9ae957b53eab30ace68b
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-09 21:08:00 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -18,7 +18,7 @@ const isSamePoint = (a, b) => {
 }
 
 const totalWithTip = (bill, tipPercentage) => {
-  const tip = (tipPercentage * bill.subtotal)
+  const tip = tipPercentage * bill.subtotal
   return {
     tip: tip,
     subtotal: bill.subtotal,
