b46646faff0404b8f83bb6d708979266f5f87c0e
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-09 21:21:50 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -31,11 +31,9 @@ const isWinner = (o) => {
 }
 
 const updateWins = (array) => {
-  let count = 0
 for(let i = 0; i < array.length;i++) {
-  if (array[i].wins === isWinner) {
-    count++
+  if (isWinner(array[i])) {
+    return array[i].wins++
   }
 }
-return count
 }
