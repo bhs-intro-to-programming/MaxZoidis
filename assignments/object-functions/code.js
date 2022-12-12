f2568406579ea155fbdc37b42ca32ba4a07a5395
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-11 21:47:44 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -21,7 +21,7 @@ const midpoint = (p1, p2) => {
 const sumSalaries = (array) => {
   let count = 0
   for(let i = 0; i< array.length;i++) {
-    count += array[i]
+    count += array.salary
   }
   return count
 }
