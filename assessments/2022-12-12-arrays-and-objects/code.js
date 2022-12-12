5171d96432e91dd53a3a5d64fe5efda385b30420
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-12 15:23:51 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -9,9 +9,8 @@ const makeRow = () => {
 const makeBoard = () => {
   let array = []
   for (let i = 0; i < 3; i++) {
-    makeRow(array[i])
-      makeRow(array[i])
-      makeRow(array[i])
+    array.push(makeRow)
+    array.push(makeRow)
   }
   return array
 }
