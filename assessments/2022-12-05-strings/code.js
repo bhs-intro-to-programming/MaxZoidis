4e963bcec19601cd13e6a2ef092649c36f59a41e
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-14 12:04:45 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -5,3 +5,7 @@ const upToX = (s) => {
 const charactersAround = (s, index ) => {
   return s[index - 1] + s[index + 1]
 }
+
+const middle = (s) => {
+  return s.substring(s.length * 0.25, s.length * 0.75)
+}
