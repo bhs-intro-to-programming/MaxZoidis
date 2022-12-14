d523d6855cfdeaa7de24db80786aae7bd7786f91
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-14 11:38:19 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -1,3 +1,11 @@
 const needHeavyCoat = (outside, cold) => {
   return outside && cold
 }
+
+const needSunscreen = (beach, skis) => {
+  return beach || skis
+}
+
+const needMittens = (outside, warm) => {
+  return outside && !warm
+}
