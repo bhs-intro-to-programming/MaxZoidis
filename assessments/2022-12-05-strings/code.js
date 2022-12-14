b997a5317e6dfbfe0befb8dba03476cbfdfdd609
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-14 12:00:00 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -1,31 +1,3 @@
 const upToX = (s) => {
-  return s.substring(0, s.indexOf('x'))
-}
-
-const charactersAround = (s, n) => {
-  return s.substring(s[n] - 1) 
-}
-
-const middle = (s) => {
-  return s.substring(s.length / 4, s.length - s.length / 4)
-}
-
-const pair = (s1, s2) => {
-  return s1 + (' and ') + s2
-}
-
-const containsX = (s) => {
-  
-}
-
-const slug = (s1, s2, s3) => {
-  s1.toLowerCase() + s2.toLowerCase()('-') + ('-') + s3.toLowerCase
-}
-
-const capitalize = (s) => {
-  return s.substring(0,1).toUpperCase() + s.substring(1,s.length).toLowerCase()
-}
-
-const capitalizeName = (firstName) => {
-  return capitalize(firstName)
+  return s.substring(0, 'x')
 }
