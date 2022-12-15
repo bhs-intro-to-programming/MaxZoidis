1820252693faf1c9f573df17f9430013e48938cd
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-14 19:40:48 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -3,44 +3,7 @@ const emit = (a, b) => console.log(a + ', ' + b);
 const yesIfEven = (n) => {
   if (n % 2 === 0) {
     return 'yes'
-  }
-  else {
+  } else {
     return 'no'
   }
-}
-
-const countXs = (s) => {
-  let count = 0
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === 'x') {
-      count++
-    }
-  }
-  return count
-}
-
-const timesTable = (n) => {
-  for (let i = 1; i < n; i++) {
-    for (let j = 1; j < n; j++) {
-      emit(i,ji * j)
-    }
-  }
-}
-
-const containsX = (s) => {
-  for(let i = 0; i < s.length; i++)
-  if (s[i] === 'x') {
-    return true
-  }
-  else {
-    return false
-  }
-}
-
-const sumSquares = (n) => {
-  let sum = 0
-  for(let i = 0; i < n;i++) {
-    sum += i ** 2
-  }
-  return sum
 }
