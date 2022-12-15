09d5cb7d1f65a36cae56d9180380ea54e2c052f6
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-14 19:12:37 -0800

    Updating assessments/2022-12-07-control-constructs/code.js

--- assessments/2022-12-07-control-constructs/code.js
+++ assessments/2022-12-07-control-constructs/code.js
@@ -121,8 +121,8 @@ const threewayClassify = (o) => {
 
 const sumSquares = (n) => {
   let sum = 0
-  for(let i = 0; i < n;i++) {
-i ** 2
+  for (let i = 0; i < n; i++) {
+    sum += i ** 2
   }
   return sum
 }
