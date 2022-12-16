792bd049497268cd68e1f0c4b9c6a7d4203dabb5
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-15 16:29:10 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -43,7 +43,7 @@ const rowForMove = (array, n) => {
 array[index].row
 }
 
-plaeMoves = (array, moves) => {
+placeMoves = (array, moves) => {
   for(let i = 0; i < moves.length;i++) {
     placeMark(array,moves[i])
   }
