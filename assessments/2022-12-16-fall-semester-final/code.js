88272fed20d31793662ea2d09314bb9ff7c9589e
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-16 11:26:06 -0800

    Updating assessments/2022-12-16-fall-semester-final/code.js

--- assessments/2022-12-16-fall-semester-final/code.js
+++ assessments/2022-12-16-fall-semester-final/code.js
@@ -168,8 +168,8 @@ const sumOfSquares = (n) => {
 }
 
 const countXs = (s) => {
-  let count =0
-  for(let i = 0; i , s.length;i++) {
+  let count = 0
+  for (let i = 0; i, s.length; i++) {
     if (s[i] === 'x') {
       count++
     }
