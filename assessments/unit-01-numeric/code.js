909514917c094b52b244c5423d576d98f1f0d866
MaxZoidis <111394634+MaxZoidis@users.noreply.github.com>
2022-12-15 17:52:40 -0800

    Updating assessments/unit-01-numeric/code.js

--- assessments/unit-01-numeric/code.js
+++ assessments/unit-01-numeric/code.js
@@ -8,43 +8,6 @@ const G = 6.6743e-11;
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
 
-const itemsLeftOver = (people, items) => {
-  return people % items
+const itemsLeftOver = (items, people) => {
+  return items % people
 }
-
-const areaOfCircle = (radius) => {
-  return Math.PI * radius ** 2
-}
-
-const volumeOfCube = (sideLength) => {
-  return sideLength ** 3
-}
-
-const populationGrowth = (populationSize, growthRate) => {
-  return populationSize * growthRate
-}
-
-const earnedRunAverage = (earnedRuns, inningsPitched) => {
-  return (earnedRuns / inningsPitched) * 9
-}
-
-const valueOfJewels = (numDiamonds, numEmeralds, valueDiamond, valueEmerald) => {
-  return (numDiamonds * valueDiamond) + (numEmeralds * valueEmerald)
-}
-
-const payWithOvertime = (hoursWorked, hourlyRate, overtimeRate) => {
-return 
-}
-
-const firstClassPostage = (weight) =>{
-return 24 * weight + 36
-}
-
-const weightOnJupiter = (weight) => {
-  return weight * ( JUPITER_GRAVITY/EARTH_GRAVITY)
-}
-
-const gravity = (mass1, mass2, distance) => {
-  return ((mass1 * mass2)/distance**2) * G
-}
-
